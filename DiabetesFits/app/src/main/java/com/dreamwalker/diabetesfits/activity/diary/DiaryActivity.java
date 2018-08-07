18d63f1f17888cd4698f75b89f36d5771805f345
Dreamwalker <aristojeff@gmail.com>
2018-08-07 21:24:24 +0900

    diary activity added

---
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/diary/DiaryActivity.java
@@ -0,0 +1,15 @@
+package com.dreamwalker.diabetesfits.activity.diary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dreamwalker.diabetesfits.R;
+
+public class DiaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+    }
+}
