9480f8342c148014dfd6a770eab915cc8588a4c3
Dreamwalker <aristojeff@gmail.com>
2018-09-03 11:26:01 +0900

    모델 수정

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/database/model/Fitness.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/database/model/Fitness.java
@@ -6,12 +6,21 @@ import io.realm.RealmObject;
 
 public class Fitness extends RealmObject {
 
+
     private String userValue;
     private String type;
+
+    private String fitnessTime; //운동시간
+    private String distance; // 운동 거리
+    private String speed; // 운동 속도
+    private String heartRate; // 심박수 
+
     private String date;
     private String time;
     private String timestamp;
     private long longTs;
     private Date datetime;
+
+
 
 }
