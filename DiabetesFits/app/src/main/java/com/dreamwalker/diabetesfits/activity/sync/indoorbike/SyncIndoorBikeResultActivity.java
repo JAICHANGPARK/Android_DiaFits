0983c1a9aad13e95f80be3668ec340bcdaeca0ba
Dreamwalker <aristojeff@gmail.com>
2018-09-05 21:07:27 +0900

    동기화 후 데이터 처리 및 결과 뷰 추가

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/sync/indoorbike/SyncIndoorBikeResultActivity.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/sync/indoorbike/SyncIndoorBikeResultActivity.java
@@ -1,22 +1,68 @@
 package com.dreamwalker.diabetesfits.activity.sync.indoorbike;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.dreamwalker.diabetesfits.R;
+import com.dreamwalker.diabetesfits.adapter.CustomItemClickListener;
+import com.dreamwalker.diabetesfits.adapter.machine.FitnessSyncAdapter;
 import com.dreamwalker.diabetesfits.model.fitness.Fitness;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.paperdb.Paper;
 
-public class SyncIndoorBikeResultActivity extends AppCompatActivity {
+public class SyncIndoorBikeResultActivity extends AppCompatActivity  implements CustomItemClickListener {
     private static final String TAG = "SyncIndoorBikeResultAct";
 
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.coordinator)
+    RelativeLayout coordinator;
+    @BindView(R.id.toolbar)
+    Toolbar myToolbar;
+    @BindView(R.id.animation_view)
+    LottieAnimationView lottieAnimationView;
+    @BindView(R.id.animationLayout)
+    LinearLayout animationLayout;
+
+    @BindView(R.id.save)
+    ImageView saveImageView;
+
+    @BindView(R.id.home)
+    ImageView homeImageView;
+
     ArrayList<Fitness> fitnessArrayList = new ArrayList<>();
+    ArrayList<Fitness> subList;
+
+    RecyclerView.LayoutManager layoutManager;
+    FitnessSyncAdapter adapter;
+
+    int newIndex;
+    int oldIndex = 0;
+    int subIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +71,24 @@ public class SyncIndoorBikeResultActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
         Paper.init(this);
+
+        setSupportActionBar(myToolbar);
+        setStatusBar();
+
+
+        myToolbar.inflateMenu(R.menu.sync_bsm_menu);
+        myToolbar.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.save:
+                    Snackbar.make(getWindow().getDecorView().getRootView(), "툴바저장버튼", Snackbar.LENGTH_SHORT).show();
+                    return false;
+                case R.id.analysis:
+//                    startActivity(new Intent(SyncBMSResultActivity.this, AnalysisBSActivity.class));
+                    //Snackbar.make(getWindow().getDecorView().getRootView(),"툴바분석", Snackbar.LENGTH_SHORT).show();
+                    return false;
+            }
+            return false;
+        });
 
         if (Paper.book("syncIndoorBike").read("data") == null) {
             Snackbar.make(getWindow().getDecorView().getRootView(), "데이터가 없어요", Snackbar.LENGTH_SHORT).show();
@@ -36,5 +100,138 @@ public class SyncIndoorBikeResultActivity extends AppCompatActivity {
         for (Fitness f : fitnessArrayList){
             Log.e(TAG, "onCreate: " + f.getFitnessTime());
         }
+
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        newIndex = fitnessArrayList.size(); //받아온 최신 데이터 사이즈
+        Log.e(TAG, "init: newIndex -  " + newIndex);
+        oldIndex = 0;
+        // TODO: 2018-02-27 기존의 인덱스와 최신 인덱스 비교 저장이 가능해야함.
+        if (Paper.book("syncIndoorBike").read("ptr") == null) {
+            //Paper.book("syncBms").write("index", newIndex);
+            Paper.book("syncIndoorBike").write("ptr", 0);
+        } else {
+            oldIndex = Paper.book("syncIndoorBike").read("ptr"); // 기존의 데이터 사이즈
+            Log.e(TAG, "init: oldIndex -  " + oldIndex);
+        }
+
+        subIndex = newIndex - oldIndex; // 새로운 데이터와 기존 데이터 양의 차를 구해 새로 추가해야하는 개수를 구한다. - 박제창
+
+        Log.e(TAG, "init: subIndex -  " + subIndex);
+        if (subIndex == 0) {
+            runOnUiThread(() -> {
+                animationLayout.setVisibility(View.VISIBLE);
+                lottieAnimationView.playAnimation();
+                fab.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.GONE);
+            });
+
+            //dbTextView.setVisibility(View.GONE);
+            // TODO: 2018-02-27 같은 데이터 양이라면
+//            Log.e(TAG, "init: 같은 데이터 양이라면");
+//            subList = new ArrayList<>(mBSList.subList(oldIndex, newIndex));
+//            for (int i = 0; i < subList.size(); i++) {
+//                Log.e(TAG, "subList - " + subList.get(i).getBsValue());
+//            }
+//            for (int i = 0; i < subList.size(); i++) {
+//                date[i] = subList.get(i).getBsTime().split(",")[0];
+//                time[i] = subList.get(i).getBsTime().split(",")[1];
+//                Log.e(TAG, "init: mBSList - " + subList.get(i).getBsValue() + subList.get(i).getBsTime());
+//            }
+//            adapter = new BSMSyncAdapter(this, subList);
+//            recyclerView.setAdapter(adapter);
+            //추가할 데이터가 없어요
+        } else if (subIndex > 0) {
+
+            runOnUiThread(() -> {
+                animationLayout.setVisibility(View.GONE);
+                lottieAnimationView.cancelAnimation();
+                fab.setVisibility(View.VISIBLE);
+                recyclerView.setVisibility(View.VISIBLE);
+            });
+
+            // TODO: 2018-02-27 새로운 데이터가 더 많다면
+            Log.e(TAG, "새로운 데이터가 더 많다면");
+            //List<BloodSugar> subList =  mBSList.subList(oldIndex, newIndex);
+            subList = new ArrayList<>(fitnessArrayList.subList(oldIndex, newIndex));
+            // subList.add(mBSList.get(mBSList.size() - 1));
+            for (int i = 0; i < subList.size(); i++) {
+                Log.e(TAG, "subList - " + subList.get(i).getDistance());
+            }
+
+//            for (int i = 0; i < subList.size(); i++) {
+//                date[i] = subList.get(i).getBsTime().split(",")[0];
+//                time[i] = subList.get(i).getBsTime().split(",")[1];
+//                Log.e(TAG, "init: mBSList - " + subList.get(i).getBsValue() + subList.get(i).getBsTime());
+//            }
+
+            adapter = new FitnessSyncAdapter(this, subList);
+            recyclerView.setAdapter(adapter);
+            // TODO: 2018-07-25 객체 생성하고 리스너 추가
+            adapter.setCustomItemClickListener(this);
+
+        } else if (subIndex < 0) {
+            // TODO: 2018-02-27 기존 데이터가 더 많다면 ( 경우가 없을 듯)
+        }
+
+
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    //tapBarMenu.setVisibility(View.VISIBLE);
+                    fab.show();
+                }
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (dy > 0 || dy < 0 && fab.isShown()) {
+                    fab.hide();
+                    //tapBarMenu.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+    private void setStatusBar() {
+        Window window = this.getWindow();
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.white));
+    }
+
+    @Override
+    public void onItemClick(View v, int position) {
+
+    }
+
+    @Override
+    public void onItemLongClick(View v, int position) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sync_bsm_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save:
+                return true;
+            case R.id.analysis:
+                return true;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/adapter/machine/FitnessSyncAdapter.java
@@ -0,0 +1,162 @@
+package com.dreamwalker.diabetesfits.adapter.machine;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.dreamwalker.diabetesfits.R;
+import com.dreamwalker.diabetesfits.adapter.CustomItemClickListener;
+import com.dreamwalker.diabetesfits.consts.GlucoseType;
+import com.dreamwalker.diabetesfits.model.fitness.Fitness;
+
+import java.util.ArrayList;
+
+/**
+ * Created by KNU2017 on 2018-02-27.
+ */
+
+
+public class FitnessSyncAdapter extends RecyclerView.Adapter<FitnessSyncAdapter.FitnessSyncViewHolder> {
+
+    Context context;
+    ArrayList<Fitness> fitnessArrayList;
+    CustomItemClickListener customItemClickListener;
+
+
+    public FitnessSyncAdapter(Context context, ArrayList<Fitness> bloodSugarArrayList) {
+        this.context = context;
+        this.fitnessArrayList = bloodSugarArrayList;
+    }
+
+    @Override
+    public FitnessSyncViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View itemView = layoutInflater.inflate(R.layout.item_bsm_sync_data, parent, false);
+        return new FitnessSyncViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(FitnessSyncViewHolder holder, int position) {
+
+        int bsType;
+
+        bsType = Integer.valueOf(fitnessArrayList.get(position).getType());
+
+        if (bsType == 0) {
+            Glide.with(context).load(R.drawable.img_unknown).into(holder.categoryImage);
+            holder.typeText.setText("Unknown");
+        } else if (bsType == 1) {
+            Glide.with(context).load(R.drawable.apple_before).into(holder.categoryImage);
+            holder.typeText.setText("식전");
+        } else if (bsType == 2) {
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+            holder.typeText.setText("식후");
+        } else if (bsType == 3) {
+            Glide.with(context).load(R.drawable.img_wake_up).into(holder.categoryImage);
+            holder.typeText.setText("공복");
+        }
+        // TODO: 2018-07-25 수정 및 추가 리사이클러 업데이트를 위해서  - 박제창
+        else if (bsType == GlucoseType.TYPE_FASTING) {
+            holder.typeText.setText(GlucoseType.FASTING);
+            Glide.with(context).load(R.drawable.img_wake_up).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_SLEEP) {
+            holder.typeText.setText(GlucoseType.SLEEP);
+            Glide.with(context).load(R.drawable.img_wake_up).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_BREAKFAST_BEFORE) {
+            holder.typeText.setText(GlucoseType.BREAKFAST_BEFORE);
+            Glide.with(context).load(R.drawable.apple_before).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_BREAKFAST_AFTER) {
+            holder.typeText.setText(GlucoseType.BREAKFAST_AFTER);
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_LUNCH_BEFORE) {
+            holder.typeText.setText(GlucoseType.LUNCH_BEFORE);
+            Glide.with(context).load(R.drawable.apple_before).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_LUNCH_AFTER) {
+            holder.typeText.setText(GlucoseType.LUNCH_AFTER);
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_DINNER_BEFORE) {
+            holder.typeText.setText(GlucoseType.DINNER_BEFORE);
+            Glide.with(context).load(R.drawable.apple_before).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_DINNER_AFTER) {
+            holder.typeText.setText(GlucoseType.DINNER_AFTER);
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_FITNESS_BEFORE) {
+            holder.typeText.setText(GlucoseType.FITNESS_BEFORE);
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+        }
+
+        else if (bsType == GlucoseType.TYPE_FITNESS_AFTER) {
+            holder.typeText.setText(GlucoseType.FITNESS_AFTER);
+            Glide.with(context).load(R.drawable.apple_after).into(holder.categoryImage);
+        }
+
+
+        holder.valueText.setText(fitnessArrayList.get(position).getDistance());
+        holder.dateTextValue.setText(fitnessArrayList.get(position).getTimestamp());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return fitnessArrayList.size();
+    }
+
+    public void setCustomItemClickListener(CustomItemClickListener listener) {
+        this.customItemClickListener = listener;
+    }
+
+
+    public class FitnessSyncViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView categoryImage;
+        //    TextView subjectText, typeText;
+//    TextView valueTitleText, valueText;
+//    TextView dateText, dateTextValue;
+        TextView dateTextValue;
+        TextView subjectText, typeText;
+        TextView valueText;
+
+        public FitnessSyncViewHolder(View itemView) {
+            super(itemView);
+
+            categoryImage = itemView.findViewById(R.id.image_view);
+            subjectText = itemView.findViewById(R.id.subjectText);
+            typeText = itemView.findViewById(R.id.typeText);
+            //valueTitleText = itemView.findViewById(R.id.valueTitleText);
+            valueText = itemView.findViewById(R.id.valueText);
+            //dateText = itemView.findViewById(R.id.dateText);
+            dateTextValue = itemView.findViewById(R.id.dateTextValue);
+
+            itemView.setOnClickListener(this);
+        }
+
+        // TODO: 2018-07-25 리사이클러 데이터 수정을 위해서 리스너 추가합니다. - 박제창
+        @Override
+        public void onClick(View v) {
+            if (customItemClickListener != null) {
+                customItemClickListener.onItemClick(v, getAdapterPosition());
+            }
+        }
+    }
+}
