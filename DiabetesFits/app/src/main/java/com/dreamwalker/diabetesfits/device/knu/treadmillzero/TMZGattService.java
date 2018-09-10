61f02b85a54114079e896a48b149acb0307a5bbe
Dreamwalker <aristojeff@gmail.com>
2018-09-10 13:48:31 +0900

    service added

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/device/knu/treadmillzero/TMZGattService.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/device/knu/treadmillzero/TMZGattService.java
@@ -1,4 +1,14 @@
 package com.dreamwalker.diabetesfits.device.knu.treadmillzero;
 
+import java.util.UUID;
+
 public class TMZGattService {
+
+    public static String HEART_RATE_MEASUREMENT = "00002a37-0000-1000-8000-00805f9b34fb"; //char
+    public static String CLIENT_CHARACTERISTIC_CONFIG = "00002902-0000-1000-8000-00805f9b34fb";
+
+    //Service
+    public final static UUID BLE_HEART_RATE             = UUID.fromString("0000180D-0000-1000-8000-00805f9b34fb");
+    public final static UUID BLE_FITNESS_MACHINE        = UUID.fromString("00001826-0000-1000-8000-00805f9b34fb");
+    
 }
