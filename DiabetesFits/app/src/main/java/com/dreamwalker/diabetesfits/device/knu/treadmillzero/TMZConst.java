71e3d7f7fa221a2728b96b287ed0694fcb420fd4
Dreamwalker <aristojeff@gmail.com>
2018-09-11 10:08:22 +0900

    const 추가

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/device/knu/treadmillzero/TMZConst.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/device/knu/treadmillzero/TMZConst.java
@@ -1,4 +1,6 @@
 package com.dreamwalker.diabetesfits.device.knu.treadmillzero;
 
 public class TMZConst {
+    public static final String DEVICE_NAME = "KNU TM0";
+    public static final byte SYNC_ALL_DATA = 0x00;
 }
