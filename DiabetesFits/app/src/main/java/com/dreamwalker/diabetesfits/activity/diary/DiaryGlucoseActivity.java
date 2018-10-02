76049091cf3755754cb9c8294c17ca41a784be28
Dreamwalker <aristojeff@gmail.com>
2018-10-02 18:01:38 +0900

    layout test

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/diary/DiaryGlucoseActivity.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/diary/DiaryGlucoseActivity.java
@@ -1,13 +1,26 @@
 package com.dreamwalker.diabetesfits.activity.diary;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.bottomappbar.BottomAppBar;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.dreamwalker.diabetesfits.R;
 import com.dreamwalker.diabetesfits.database.RealmManagement;
 import com.dreamwalker.diabetesfits.database.model.Glucose;
+import com.dreamwalker.horizontalcalendar.HorizontalCalendar;
+import com.dreamwalker.horizontalcalendar.HorizontalCalendarView;
+import com.dreamwalker.horizontalcalendar.utils.HorizontalCalendarListener;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
@@ -15,6 +28,14 @@ import io.realm.RealmResults;
 public class DiaryGlucoseActivity extends AppCompatActivity {
     private static final String TAG = "DiaryGlucoseActivity";
 
+    @BindView(R.id.calendarView)
+    HorizontalCalendarView calendarView;
+    @BindView(R.id.bottomAppBar)
+    BottomAppBar bottomAppBar;
+    @BindView(R.id.fab)
+    FloatingActionButton floatingActionButton;
+
+    HorizontalCalendar horizontalCalendar;
     Realm realm;
     RealmConfiguration realmConfiguration;
 
@@ -24,6 +45,44 @@ public class DiaryGlucoseActivity extends AppCompatActivity {
         setContentView(R.layout.activity_diary_glucose);
         initSetting();
 
+
+        /* start 2 months ago from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.MONTH, -2);
+
+        /* end after 2 months from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.MONTH, 2);
+
+        // Default Date set to Today.
+        final Calendar defaultSelectedDate = Calendar.getInstance();
+
+        horizontalCalendar = new HorizontalCalendar.Builder(this,R.id.calendarView)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .configure()
+                .formatTopText("MMM")
+                .formatMiddleText("dd")
+                .formatBottomText("EE")
+                .showTopText(true)
+                .showBottomText(true)
+                .textColor(Color.LTGRAY, Color.WHITE)
+                .colorTextMiddle(Color.LTGRAY, Color.parseColor("#ffd54f"))
+                .end()
+                .defaultSelectedDate(defaultSelectedDate)
+                .build();
+
+        Log.e("Default Date", DateFormat.format("EEE, MMM d, yyyy", defaultSelectedDate).toString());
+
+        horizontalCalendar.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                String selectedDateStr = DateFormat.format("EEE, MMM d, yyyy", date).toString();
+                Toast.makeText(DiaryGlucoseActivity.this, selectedDateStr + " selected!", Toast.LENGTH_SHORT).show();
+                Log.i("onDateSelected", selectedDateStr + " - Position = " + position);
+            }
+
+        });
 
         RealmResults<Glucose> result = realm.where(Glucose.class).findAll();
         Log.e(TAG, "onCreate: " + result.size() );
@@ -35,6 +94,15 @@ public class DiaryGlucoseActivity extends AppCompatActivity {
 
     private void initSetting(){
         initRealm();
+        bindView();
+        initToolbar();
+    }
+    private void initToolbar(){
+        setSupportActionBar(bottomAppBar);
+        bottomAppBar.setFabAlignmentMode(BottomAppBar.FAB_ALIGNMENT_MODE_END);
+    }
+    private void bindView(){
+        ButterKnife.bind(this);
     }
     private void initRealm(){
         Realm.init(this);
@@ -49,5 +117,10 @@ public class DiaryGlucoseActivity extends AppCompatActivity {
         Realm.getInstance(realmConfiguration).close();
         realm.close();
         super.onDestroy();
+    }
+
+    @OnClick(R.id.fab)
+    public void onClickedFab(){
+
     }
 }
