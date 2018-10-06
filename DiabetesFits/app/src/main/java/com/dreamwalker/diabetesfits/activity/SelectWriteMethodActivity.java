234ca4b2ccf5da1fde06d869f063fb4da129a280
Dreamwalker <aristojeff@gmail.com>
2018-10-07 00:48:24 +0900

    method activity added

---
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/SelectWriteMethodActivity.java
@@ -0,0 +1,13 @@
+package com.dreamwalker.diabetesfits.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectWriteMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_write_method);
+    }
+}
