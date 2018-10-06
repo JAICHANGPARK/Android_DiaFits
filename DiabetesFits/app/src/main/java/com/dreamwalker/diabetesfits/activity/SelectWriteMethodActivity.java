ec276e51b5445150461aaac6a3a19c763a813442
Dreamwalker <aristojeff@gmail.com>
2018-10-07 01:26:37 +0900

    paper work

--- DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/SelectWriteMethodActivity.java
+++ DiabetesFits/app/src/main/java/com/dreamwalker/diabetesfits/activity/SelectWriteMethodActivity.java
@@ -1,7 +1,9 @@
 package com.dreamwalker.diabetesfits.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.dreamwalker.diabetesfits.R;
 
 public class SelectWriteMethodActivity extends AppCompatActivity {
 
